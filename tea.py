802f581883fa6c380bb66bdfc47431cc07072f64
amerenothing <141737892+amerenothing@users.noreply.github.com>
2023-08-08 16:35:20 +0300

    Update tea.py

--- tea.py
+++ tea.py
@@ -1,17 +1,2 @@
-from bot.bot import Bot
-from bot.handler import MessageHandler
-
-TOKEN = "001.3154942192.1068193318:1011019675"
-Bot_id = 1011019675
-Bot_Name = "Voronezh36TeaBestbot"
-api_url_base="https://api.icq.net/bot/v1"
-
-bot = Bot(token=TOKEN)
-
-def message_cb(bot, event):
-    bot.send_text(chat_id=event.from_chat, text=event.text)
-
-bot.dispatcher.add_handler(MessageHandler(callback=message_cb))
-bot.start_polling()
-bot.idle()
+import vk_api
 
