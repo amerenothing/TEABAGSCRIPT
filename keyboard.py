48b2049c594966d3afa97b669f85833dd97a9bde
amerenothing <141737892+amerenothing@users.noreply.github.com>
2023-08-09 16:35:51 +0300

    1

---
+++ keyboard.py
@@ -0,0 +1,61 @@
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(456, 427)
+        MainWindow.setAutoFillBackground(False)
+        MainWindow.setStyleSheet("background-color: rgb(255, 255, 255);")
+        MainWindow.setWindowFilePath("")
+        MainWindow.setDocumentMode(False)
+        MainWindow.setDockNestingEnabled(False)
+        MainWindow.setUnifiedTitleAndToolBarOnMac(False)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 360, 441, 41))
+        font = QtGui.QFont()
+        font.setFamily("Georgia")
+        font.setPointSize(22)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("background-color: rgb(225, 255, 205);")
+        self.pushButton.setObjectName("pushButton")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(0, 0, 211, 51))
+        font = QtGui.QFont()
+        font.setFamily("Script")
+        font.setPointSize(18)
+        self.label_2.setFont(font)
+        self.label_2.setStyleSheet("background-color: rgb(229, 255, 213);")
+        self.label_2.setObjectName("label_2")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(3, 53, 451, 301))
+        self.textEdit.setObjectName("textEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "START"))
+        self.label_2.setText(_translate("MainWindow", "LIVNY COMPANY PRODUCTION"))
+        self.textEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
--- tea.py
+++ tea.py
@@ -1,26 +1,13 @@
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from PyQt5 import QtCore, QtGui, QtWidgets
+from keyboard import Ui_MainWindow
+import sys
 
-
-TOKEN = "vk1.a.hEEDGvZeT662vl6cm84AG7Pp-DlAnUnBfuN9GniNo0v1CldwaUG64n_mFk_uueI4BjNEDJAObcDr4yXHOev-t28jDCmhjXwC6tTPfyS46xYBwzvSp6O9GCSHfpQEHblDUQkWQ0q2vJqaJQquMoMEoZuTbGMRKOesntWUsAwJcpRoA8k12wZzUTY7Rqr6eCGBP8YI1uwJDOXlJCbpHDPu1w"
-
-vk_session = vk_api.VkApi(token=TOKEN)
-session_api = vk_session.get_api()
-longpoll = VkLongPoll(vk_session)
-
-
-def send_some_msg(id, msg):
-    vk_session.method("messages.send", {"user_id": id, "message":msg, "random_id":0})
-
-
-
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
-            msg = event.text
-            id = event.user_id
-            send_some_msg(id, msg)
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
 
 
 
